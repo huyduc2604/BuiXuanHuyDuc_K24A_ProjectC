78404473e4c063eea781d1e011754846643cf028
huyduc2604 <bui.duc2604@gmail.com>
2025-02-12 13:42:43 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "datatype.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int choose, countBook=0;
+	do{
+		showMenuBook();
+		scanf("%d", &choose);
+		getchar();
+		switch(choose){
+			case 1:
+				addBook(&countBook);
+				break;
+			case 2:
+				showBook(countBook);
+				break;
+			case 3:
+				editBook(countBook);
+                break; 
+            case 4:
+				deleteBook(countBook);
+                break;
+            case 5:
+                sortBooks(countBook);
+                break;
+            case 6:
+               	searchBook(countBook);
+            case 0:
+                printf("Thoat chuong trinh. Tam biet!\n");
+                return;
+            default:
+                printf("Lua chon khong hop le. Vui long chon lai.\n"); 
+		}
+	}while(choose!=0);
+	return 0;
+}
